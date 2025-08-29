023e3a82b3109be4e6cf15bf4aa46ba91e207f2f
JV3005 <juanvega3005@gmail.com>
2025-08-29 07:02:08 -0400

    Add migrations for propietarios and documentos tables
    Introduces two new migration files: one for creating the 'propietarios' table with a foreign key to 'documentos', and another for creating the 'documentos' table with a foreign key to 'tipo_doc'. These migrations set up the necessary schema for managing propietarios and their associated documents.

---
+++ theftp/database/migrations/2025_08_29_105958_create_documentos_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('documentos', function (Blueprint $table) {
+            $table->id();
+            $table->text('url');
+            $table->text('observaciones');
+            $table-> foreignId('tipo_doc_id')->constrained('tipo_doc');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('documentos');
+    }
+};
