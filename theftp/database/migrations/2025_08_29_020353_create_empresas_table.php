1fc15cdf76336d6547e8cb6edbff8c642ee241b2
JV3005 <juanvega3005@gmail.com>
2025-08-28 21:13:30 -0500

    Add migrations for empresas, conductores, and related tables
    Introduces new migration files to create the tipo_empresa, empresas, empresa_usuario, and conductores tables. These migrations establish the necessary schema and foreign key relationships for managing companies, their types, user associations, and drivers.

---
+++ theftp/database/migrations/2025_08_29_020353_create_empresas_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('empresas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nit');
+            $table->string('name');
+            $table->foreignId('tipo_empresa_id')->constrained('tipo_empresa');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('empresas');
+    }
+};
