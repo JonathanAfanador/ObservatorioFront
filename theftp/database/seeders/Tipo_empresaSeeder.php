efc689d727bfbe67b17e256ed75656db53d399ab
JV3005 <juanvega3005@gmail.com>
2025-10-07 02:58:23 -0500

    Add seeders for user and company type tables
    Introduces Tipo_empresaSeeder and UserSeeder to populate the 'users' table with base and fake users, and sets up a placeholder for seeding company types. Ensures users are assigned to available personas and valid roles, improving initial database setup for development and testing.

---
+++ theftp/database/seeders/Tipo_empresaSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class Tipo_empresaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
