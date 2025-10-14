c3a556b96b84eabc453c3ca7ff055ae76fb14219
Carrotw <91509157+SantiHSilva@users.noreply.github.com>
2025-10-14 16:57:45 -0500

    Ordenamiento de migraciones

--- theftp/database/migrations/2025_09_02_002301_create_seguim_gps_table.php
+++ theftp/database/migrations/2025_09_02_002301_create_seguim_gps_table.php
@@ -14,13 +14,16 @@ return new class extends Migration
     {
         Schema::create('seguim_gps', function (Blueprint $table) {
             $table->id();
-            $table->geography('ubicacion', subtype: 'point', srid: 4326)->nullable();
+            // TODO: Misma pregunta https://trello.com/c/zbWGJOYp/4-indagar-por-la-tabla-rutas-si-es-solo-renderizado-o-tiene-otros-usos-relevantes-planteados
+            // $table->geography('ubicacion', subtype: 'point', srid: 4326)->nullable();
+            $table->float('latitud')->nullable();
+            $table->float('longitud')->nullable();
             $table->timestamp('fecha_hora')->nullable();
             $table->boolean('deleted_at')->default(false);
             $table->foreignId('vehiculo_id')->constrained('vehiculo');
             $table->timestamps();
         });
-        DB::statement('CREATE INDEX idx_ubicacion ON seguim_gps USING GIST (ubicacion);');
+        // DB::statement('CREATE INDEX idx_ubicacion ON seguim_gps USING GIST (ubicacion);');
     }
 
     /**
