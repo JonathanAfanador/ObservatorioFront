8959ea334b6ecc7a60ce82f063873750ff13464b
JV3005 <juanvega3005@gmail.com>
2025-10-16 15:06:02 -0500

    Add departamentos API routes and improve comments
    Introduced new API routes for departamentos: paginacion, creacion, actualizacion, eliminacion, and restauracion, all protected by auth:sanctum middleware. Also improved comments and structure for better clarity in the API routes file.

--- theftp/routes/api.php
+++ theftp/routes/api.php
@@ -2,14 +2,16 @@
 
 use App\Enums\Acciones;
 use App\Enums\Tablas;
+use App\Http\Controllers\DepartamentosController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\V1\AuthController;
 use App\Http\Services\PermisosService;
 use Illuminate\Support\Facades\Auth;
 
+// Rutas API
 Route::get('/test', function (){
-
+    // Definir los permisos a verificar
     $permisos = [
         [
             'tabla' => Tablas::USERS,
@@ -26,15 +28,15 @@ Route::get('/test', function (){
             ],
         ]
     ];
-
+    // Verificar permisos
     try{
-        
+
         PermisosService::verificarPermisos($permisos);
 
         return response()->json([
             'message' => 'Permisos verificados correctamente'
         ], 200);
-
+        // Si no tiene permisos, se lanza una excepción y se captura aquí
     } catch (Exception $e){
         return response()->json([
             'message' => $e->getMessage()
@@ -43,6 +45,7 @@ Route::get('/test', function (){
 
 })->middleware('auth:sanctum');
 
+// Ruta para obtener el usuario autenticado con sus relaciones
 Route::get('/user', function (Request $request) {
     $usuario = Auth::user();
 
@@ -52,5 +55,18 @@ Route::get('/user', function (Request $request) {
 
 })->middleware('auth:sanctum');
 
+//TODO: Rutas para departamentos
+// Paginacion
+Route::get('/paginacion', [DepartamentosController::class, 'departamentos_paginados'])->middleware('auth:sanctum');
+// Creacion
+Route::post('/creacion', [DepartamentosController::class, 'crear_departamento'])->middleware('auth:sanctum');
+// Actualizacion
+Route::put('/actualizacion', [DepartamentosController::class, 'actualizar_departamento'])->middleware('auth:sanctum');
+// Eliminacion (Deshabilitacion)
+Route::delete('/eliminacion', [DepartamentosController::class, 'eliminar_departamento'])->middleware('auth:sanctum');
+// Restauracion (Habilitacion)
+Route::post('/restauracion', [DepartamentosController::class, 'restaurar_departamento'])->middleware('auth:sanctum');
+
+// Registro y Login
 Route::post('/register', [AuthController::class, 'registro']);
 Route::post('/login', [AuthController::class, 'login']);
