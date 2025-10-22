2d9071a30abd12426d4ac12185e2c644a7e55e45
Carrotw <91509157+SantiHSilva@users.noreply.github.com>
2025-10-21 23:18:28 -0500

    Customize guest redirect and auth exception handling
    Adds logic to redirect guests to the login route for non-API requests and returns a JSON response for authentication exceptions on API routes. Improves user experience and API error handling.

--- theftp/bootstrap/app.php
+++ theftp/bootstrap/app.php
@@ -1,8 +1,10 @@
 <?php
 
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,10 +14,21 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //$middleware->api(prepend: [
-        //    \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-        //]);
+        $middleware->redirectGuestsTo(function (Request $request) {
+            if ($request->is('api/*')) {
+                return null;
+            } else {
+                return route('login');
+            }
+        });
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->render(function (AuthenticationException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'status' => false,
+                    'message' => "La autenticación es requerida para acceder a este recurso.",
+                ], 401);
+            }
+        });
     })->create();
